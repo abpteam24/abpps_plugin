e5d6ec278efea1dc9322a249f768d8b85c78fe37
abpteam24 <156562143+abpteam24@users.noreply.github.com>
2024-10-30 14:53:42 +0600

    Update ABPPS_Plugin.php

--- ABPPS_Plugin.php
+++ ABPPS_Plugin.php
@@ -1,11 +1,11 @@
 <?php
 /**
 * Plugin Name: ABP Team PDF Tools
- * Plugin URI: http://abpteam24.com
+ * Plugin URI: http://abp-wp.com
 * Description: This is the  PDF Tools for PDF Ticket, This plugin needs to create pdf file.
  * Version: 1.0.0
  * Author: ABP Team
- * Author URI: http://www.abpteam24.com/
+ * Author URI: http://www.abp-wp.com/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: abpps_plugin
@@ -14,5 +14,13 @@
  */
 if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
 if( ! class_exists( 'mPDF' ) ) {
-    require_once( dirname(__FILE__) . "/lib/vendor/autoload.php");
+	if (!class_exists('ABPPS_PDF_Support')) {
+		class ABPPS_PDF_Support {
+			public function __construct() {
+				require_once (dirname(__FILE__) . "/lib/vendor/autoload.php");
+			}
+
+		}
+		new ABPPS_PDF_Support();
+	}
 }
